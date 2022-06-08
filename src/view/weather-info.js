3c6bfe744a06ec01ad7c33eaeee3dfbd4a0760ea
Alex Jongman <31511625+Alex-Jongman@users.noreply.github.com>
2022-06-08 14:13:03 +0200

    Initial commit

---
+++ src/view/weather-info.js
@@ -0,0 +1,19 @@
+import WeatherService from '../service/weather_service';
+import { round } from '../utils/weather-utils';
+
+const weatherService = new WeatherService();
+
+/**
+ * This function should render the HTML element with the class '.weather-info'.
+ * The information to be shown should be an icon representing the current weather for a given city.
+ * Followed by the name of the city and the current temperature in degrees celsius.
+ *
+ * @function renderWeatherInfo
+ *
+ */
+function renderWeatherInfo() {
+  const city = 'Utrecht';
+}
+
+// ------------- Main program --------------------
+renderWeatherInfo();
